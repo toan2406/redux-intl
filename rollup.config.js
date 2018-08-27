aca6edb3f984e29cbaee2eda51d392f38dadba34
Toan Nguyen <toan2406@gmail.com>
2018-08-27 11:29:09 +0700

    feat: setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: 'lib/index.js', format: 'cjs' },
+    { file: 'lib/index.es.js', format: 'es' },
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+  external: ['react'],
+};

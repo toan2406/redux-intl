d730e69331048606f73527c209b818ba82187700
Toan Nguyen <toan2406@gmail.com>
2018-12-21 14:30:14 +0700

    chore: allow set store more than once

--- src/helpers/Intl.js
+++ src/helpers/Intl.js
@@ -1,6 +1,7 @@
 import invariant from 'invariant';
 import { createFormatters } from '../injectIntl';
 
+let unsubscribe = () => {};
 let intlObject = null;
 
 const getFormatter = formatterName => (...params) => {
@@ -10,12 +11,11 @@ const getFormatter = formatterName => (...params) => {
 
 const Intl = {
   setStore: store => {
-    invariant(!intlObject, '[Redux-Intl] Intl helper is already configurated');
-
     intlObject = createFormatters(store.getState().intl);
     let currentLocale = intlObject.locale;
 
-    store.subscribe(() => {
+    unsubscribe();
+    unsubscribe = store.subscribe(() => {
       const previousLocale = currentLocale;
       const currentState = store.getState();
       currentLocale = currentState.intl.locale;
